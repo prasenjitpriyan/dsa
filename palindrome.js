7e60a54c12c22944df1b555267b07f74fc744cbc
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-09 20:36:40 +0530

    Palindrome number added

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+// Question 1 - Palindrome Number
+// An integer is a palindrome when it reads the same forward and backward
+
+//Input: x = 121 ------>>>>> Output: true
+//Input: x = -121 ------>>>>> Output: false
+//Input: x = 10 ------>>>>> Output: false
+
+const isPalindrome = (x) => {
+  return x < 0 ? false : x === +x.toString().split('').reverse().join('');
+}
+
+const response = isPalindrome(121);
+console.log(response);
+
+// 121 => "121" => ["1","2","1"] => ["1", "2","1"] => "121"
